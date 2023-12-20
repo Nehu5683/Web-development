54722e4b5a14afc08ab99d374a71e809450767fe
Nehu5683 <nehayogi444gmail.com>
2023-12-20 21:37:46 +0530

    initial file

--- script/script.js
+++ script/script.js
@@ -1,6 +1,19 @@
+// const max= prompt("Range of guess number:- ");
+// const random= Math.floor(Math.random()*max)+1;
 
-// myscript.js
-document.getElementById("myButton").addEventListener("click", function() {
-    alert("Button Clicked!");
-  });
-  `` 
+// let guess= prompt(" guess number:- ");
+
+// while(true){
+//     if(guess == "Quite"){
+//         console.log("User Quiting the game");
+//         break;
+//     }
+//     else if(guess == random){
+//         console.log("Congratulation!! You guess the write number!!");
+//         console.log("Congratulation!! you win 10 rupees");
+//         break;
+//     }
+//     else{
+//    guess= prompt(" try again you guess wrong number:- "); 
+//     }
+// } 
